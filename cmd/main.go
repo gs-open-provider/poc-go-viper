5487c41b978fc8b133e7d31218d7639ef89c44bf
prashanth-gs <prashanth@gapstars.net>
2019-05-28 10:47:25 +0530

    main file implemented to demo viper

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	i "github.com/gs-open-provider/poc-go-viper/internal"
+	c "github.com/gs-open-provider/poc-go-viper/models"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	// Initialize Viper across the application
+	i.InitializeViper()
+
+	var configuration c.Configurations
+
+	// Set undefined variables
+	viper.SetDefault("database.dbname", "test_db")
+
+	err := viper.Unmarshal(&configuration)
+	if err != nil {
+		fmt.Printf("Unable to decode into struct, %v", err)
+	}
+
+	// Reading variables using the model
+	fmt.Println("Reading variables using the model..")
+	fmt.Println("Database is\t", configuration.Database.DBName)
+	fmt.Println("Port is\t\t", configuration.Server.Port)
+	fmt.Println("EXAMPLE_PATH is\t", configuration.EXAMPLE_PATH)
+	fmt.Println("EXAMPLE_VAR is\t", configuration.EXAMPLE_VAR)
+
+	// Reading variables without using the model
+	fmt.Println("\nReading variables without using the model..")
+	fmt.Println("Database is\t", viper.GetString("database.dbname"))
+	fmt.Println("Port is\t\t", viper.GetInt("server.port"))
+	fmt.Println("EXAMPLE_PATH is\t", viper.GetString("EXAMPLE_PATH"))
+	fmt.Println("EXAMPLE_VAR is\t", viper.GetString("EXAMPLE_VAR"))
+	fmt.Println("PATH var is\t", viper.GetString("path"))
 }
